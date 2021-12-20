858314808951c052e016d52e39bd18152b2bb672
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2021-12-20 18:03:07 -0500

    Se organiza el Set de productosId en venta

--- src/main/java/co/com/sofka/dulceria/tienda/Venta.java
+++ src/main/java/co/com/sofka/dulceria/tienda/Venta.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public class Venta extends Entity<VentaId> {
 
-    protected Set<ProductoId>;
+    protected Set<ProductoId> productos;
     protected TiendaId tiendaId;
     protected CajeroId cajeroId;
     protected VendedorId vendedorId;
