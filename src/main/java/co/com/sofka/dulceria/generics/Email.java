3985d30cb56a6734b49486654fc8934ee2156536
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2021-12-20 22:45:22 -0500

    Se añaden las validaciones a los VO generics

--- src/main/java/co/com/sofka/dulceria/generics/Email.java
+++ src/main/java/co/com/sofka/dulceria/generics/Email.java
@@ -1,4 +1,41 @@
 package co.com.sofka.dulceria.generics;
 
-public class Email {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Email implements ValueObject<String> {
+
+    private final String value;
+
+    public Email(String value){
+        this.value = Objects.requireNonNull(value);
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("El email no puede estar vacío");
+        }
+        if(this.value.length()<3){
+            throw new IllegalArgumentException("El email no puede tener menos de cuatro caractéres");
+        }
+//        if(!value.matches(){ //TERMINAR
+//            throw new IllegalArgumentException("El email no es válido");
+//        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email = (Email) o;
+        return Objects.equals(value, email.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
 }
